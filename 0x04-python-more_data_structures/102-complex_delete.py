afb2cb1a70ce14f4afd40d91df0e5228fd0ff0e1
InioluwaAfolabi <afolabiinioluwa504@gmail.com>
2023-01-11 14:16:39 -0800

    commit corrections

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(my_dict, value):
     keys_to_del = []
-    for key in my_dict
+    for key in my_dict:
         if my_dict[key] == value:
             keys_to_del.append(key)
     for key in keys_to_del:
